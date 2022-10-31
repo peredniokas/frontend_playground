8d7008253c48bd360021c1a69a432fd3ad01774b
peredniokas <you@example.com>
2022-10-31 08:00:25 +0200

    flask

---
+++ flask_styling.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("styling/home.html")
+
+@app.route("/containers")
+def containers():
+    return render_template("styling/containers.html")
+
+@app.route("/grid")
+def grid():
+    return render_template("styling/grid.html")
+
+@app.route("/page")
+def fullpage():
+    return render_template("styling/fullpage.html")
+
+@app.route("/flex")
+def flex():
+    return render_template("styling/flex.html")
+
+@app.route("/flexpage")
+def flexpage():
+    return render_template("styling/flexpage.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
